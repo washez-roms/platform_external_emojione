16ac02408655f2319e803d4afbe73d89c706c101
Pietro Di Gennaro <pietrodig@live.com>
2017-02-06 17:47:28 +0100

    Initial module import.

---
+++ fonts.mk
@@ -0,0 +1,6 @@
+# Include these fonts on all devices
+PRODUCT_COPY_FILES += \
+    external/emojione/emojione-android.ttf:system/fonts/emojione-android.ttf
+
+PRODUCT_PACKAGES := \
+    emojione-android.ttf
